4e94089fffb35e15c1a1027aaa533f0c4868fe95
Shanawar7 <151776983+Shanawar7@users.noreply.github.com>
2024-05-28 20:15:46 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include "./src/header/Universal.h"
 #include "./src/StateMachine.cpp"
 #include "./src/header/PlayState.h"
@@ -12,6 +14,31 @@ int main()
     std::shared_ptr<SelectionState> selectionState = std::make_shared<SelectionState>();
     sm::StateMachine stateMachine(selectionState);
 
+    sf::SoundBuffer bufferW, bufferA, bufferS, bufferD, bufferQ, bufferI, bufferJ, bufferK, bufferL;
+if (!bufferW.loadFromFile("landing.wav") || !bufferA.loadFromFile("landing.wav") ||
+    !bufferS.loadFromFile("landing.wav") || !bufferD.loadFromFile("landing.wav") ||
+    !bufferQ.loadFromFile("landing.wav") || !bufferI.loadFromFile("landing.wav") ||
+    !bufferJ.loadFromFile("landing.wav") || !bufferK.loadFromFile("landing.wav") ||
+    !bufferL.loadFromFile("landing.wav")) {
+    // Handle error loading sound files
+    return -1;
+}
+
+// Create sound objects and set their buffers
+sf::Sound soundW, soundA, soundS, soundD, soundQ, soundI, soundJ, soundK, soundL;
+soundW.setBuffer(bufferW);
+soundA.setBuffer(bufferA);
+soundS.setBuffer(bufferS);
+soundD.setBuffer(bufferD);
+soundQ.setBuffer(bufferQ);
+soundI.setBuffer(bufferI);
+soundJ.setBuffer(bufferJ);
+soundK.setBuffer(bufferK);
+soundL.setBuffer(bufferL);
+
+
+
+
     sf::Clock deltaClock;
     while (window->isOpen())
     {
@@ -21,7 +48,52 @@ int main()
             stateMachine.UpdateStates(deltaClock.restart(), e);
             if (e.type == sf::Event::KeyPressed)
             {
-                if (e.key.scancode == sf::Keyboard::Scan::H)
+                 if (e.key.scancode == sf::Keyboard::Scan::W)
+            {
+                // Play sound when W is pressed
+                soundW.play();
+            }
+            else if (e.key.scancode == sf::Keyboard::Scan::A)
+            {
+                // Play sound when A is pressed
+                soundA.play();
+            }
+            else if (e.key.scancode == sf::Keyboard::Scan::S)
+            {
+                // Play sound when S is pressed
+                soundS.play();
+            }
+            else if (e.key.scancode == sf::Keyboard::Scan::D)
+            {
+                // Play sound when D is pressed
+                soundD.play();
+            }
+            else if (e.key.scancode == sf::Keyboard::Scan::Q)
+            {
+                // Play sound when Q is pressed
+                soundQ.play();
+            }
+             else if (e.key.scancode == sf::Keyboard::Scan::I)
+            {
+                // Play sound when Q is pressed
+                soundI.play();
+            }
+             else if (e.key.scancode == sf::Keyboard::Scan::J)
+            {
+                // Play sound when Q is pressed
+                soundJ.play();
+            }
+             else if (e.key.scancode == sf::Keyboard::Scan::K)
+            {
+                // Play sound when Q is pressed
+                soundK.play();
+            }
+             else if (e.key.scancode == sf::Keyboard::Scan::L)
+            {
+                // Play sound when Q is pressed
+                soundL.play();
+            }
+                else if (e.key.scancode == sf::Keyboard::Scan::H)
                 {
                     selectionState->SetVisible(false);
                     stateMachine.AddState(playState);
@@ -39,4 +111,4 @@ int main()
     return 0;
 }
 // g++ -Isrc/include -c main.cpp
-// g++ main.o -o main -Lsrc/lib -lsfml-graphics -lsfml-window -lsfml-system
+// g++ main.o -o main -Lsrc/lib -lsfml-graphics -lsfml-window -lsfml-system  -lsfml-audio
